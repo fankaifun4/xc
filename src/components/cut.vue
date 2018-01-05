2f2f1c8ec36550e45b2fdce8c9b4198d30c583b7
fankx <fankaifun4@qq.com>
2018-01-05 20:13:02 +0800

    x-00001

--- src/components/cut.vue
+++ src/components/cut.vue
@@ -13,14 +13,17 @@
 			</div>
 		</div>
 		<!-- Show the cropped image in modal -->
-        <div class="modal fade docs-cropped" id="getCroppedCanvasModal" aria-hidden="true" aria-labelledby="getCroppedCanvasTitle" role="dialog" tabindex="-1">
+        <div class="modal docs-cropped" id="getCroppedCanvasModal" 
+			v-show="isDroop"
+			aria-labelledby="getCroppedCanvasTitle" role="dialog" tabindex="-1" @click="isDroop=false">
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
-                <button class="close" data-dismiss="modal" type="button" aria-hidden="true">&times;</button>
-                <h4 class="modal-title" id="getCroppedCanvasTitle">Cropped</h4>
+                <button class="close" data-dismiss="modal" type="button" @click="isDroop=false">&times;</button>
+                <h4 class="modal-title" id="getCroppedCanvasTitle">截取图像</h4>
               </div>
-              <div class="modal-body"></div>
+              <div class="modal-body">
+			  </div>
               <!-- <div class="modal-footer">
                 <button class="btn btn-primary" data-dismiss="modal" type="button">Close</button>
               </div> -->
@@ -31,9 +34,9 @@
 </template>
 <script>
 	import '../libs/jquery.min.js'
-	import "../libs/cropper.js"
-	import "../libs/bootstrap.min.js"
-	import '../style/cropper.scss';
+	import '../libs/cropper.js'
+	import '../libs/bootstrap.min.js'
+	import '../style/cropper.scss'
 	export default{
 		props:['aspectr'],
 		data(){
@@ -56,7 +59,8 @@
 		        	aspectRatio:this.aspectr,
 					crop: function (data) {
 					}
-		        }
+				},
+				isDroop:false
 			}
 		},
 		mounted(){
@@ -67,11 +71,15 @@
 				let $=jQuery;
 				let $img=jQuery(this.$refs.cutImg);
 				let result=$img.cropper(this.options)
+				let _this=this;
 				$('.save').on('click',function(){
 					let data=$(this).data();
-					if (data.method === 'getCroppedCanvas') {
-			          $('#getCroppedCanvasModal').modal().find('.modal-body').html(result);
-			        }
+					_this.isDroop=true;
+					result = $img.cropper(data.method, data.option);
+					$('#getCroppedCanvasModal').find('.modal-body').html(result);
+				})
+				$('.reset').on('click',function(){
+					$img.cropper('reset');
 				})
 				
 			}
@@ -79,46 +87,112 @@
 
 	}
 </script>
-<style scoped lang='scss'>
-	@import '../style/comm.scss';
-	.docs-cropped .modal-body {
-	  text-align: center;
-	}
-
-	.docs-cropped .modal-body > img,
-	.docs-cropped .modal-body > canvas {
-	  max-width: 100%;
-	}
-	.ctrl{
-		color:#fff;
-		display:flex;
-		justify-content:center;
-		>div{
-			padding:15px 30px;
-			text-align: center;
-			border:1px solid #ccc;
-			border-radius: 5px;
-			margin:10px 30px;
-			font-size:32px;
-		}	
-		.save{
-			background:#090;
-		}
-		.reset{
-			background:#f20;
-		}
-		.cancel{
-			background:#ccc;
-		}
-	}
+<style  lang='scss'>
+	
 
 	.layz{
+
 		position:absolute;
 		left:0;
 		right: 0;
 		top:0;
 		bottom: 0;
 		z-index:200;
+		/**
+			** modal start
+		*/
+		.modal{
+			position: fixed;
+			top: 0;
+			right: 0;
+			bottom: 0;
+			left: 0;
+			z-index: 1040;
+			overflow: hidden;
+			outline: 0;
+			.modal-dialog{
+				position: relative;
+				width: auto;
+				margin: 10px;
+				padding-top:15%;
+				.modal-content{
+					position: relative;
+					background-color: #fff;
+					background-clip: padding-box;
+					border: 1px solid #999;
+					border: 1px solid rgba(0,0,0,.2);
+					border-radius: 6px;
+					outline: 0;
+					box-shadow: 0 3px 9px rgba(0,0,0,.5);
+					.modal-header{
+						min-height: 16.43px;
+						padding: 15px;
+						border-bottom: 1px solid #e5e5e5;
+						.modal-title {
+							margin: 0;
+							line-height: 1.42857143;
+							font-size: 32px;
+							text-align: center;
+						}
+						>button{
+							float: right;
+							font-size: 21px;
+							font-weight: 700;
+							line-height: 1;
+							color: #000;
+							text-shadow: 0 1px 0 #fff;
+							filter: alpha(opacity=20);
+							opacity: .2;
+							&.close{
+								padding: 0;
+								cursor: pointer;
+								background: 0 0;
+								border: 0;
+								margin-top: -2px;
+							}
+
+						}
+					}
+					.modal-body {
+						position: relative;
+						padding: 30px;
+						canvas{
+							max-width:100%;
+						}
+						img{
+							max-width:100%;
+						}
+					}
+				}
+			}
+		}
+		.ctrl{
+			color:#fff;
+			display:flex;
+			justify-content:center;
+			>div{
+				padding:15px 30px;
+				text-align: center;
+				border:1px solid #ccc;
+				border-radius: 5px;
+				margin:10px 30px;
+				font-size:32px;
+			}	
+			.save{
+				background:#090;
+			}
+			.reset{
+				background:#f20;
+			}
+			.cancel{
+				background:#ccc;
+			}
+		}
+
+		/**
+			** modal end
+		*/
+
 		.wrap{
 			width:100%;
 			height:100%;
